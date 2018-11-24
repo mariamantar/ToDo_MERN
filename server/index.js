d6a475e1519a05cd2147b5c56404c4de9101b794
Mariam <mariam.antar@hotmail.com>
2018-11-24 17:46:11 +1100

    first commit

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const { GraphQLServer } = require('graphql-yoga')
+
+
+// this is the schema
+const typeDefs = `
+  type Query {
+    hello(name: String): String!
+  }
+`
+
+const resolvers = {
+  Query: {
+    hello: (_, { name }) => `Hello ${name || 'World'}`,
+  },
+}
+
+const server = new GraphQLServer({ typeDefs, resolvers })
+server.start(() => console.log('Server is running on localhost:4000'))
