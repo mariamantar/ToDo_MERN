042944160bcc598212720f7bc49a8338a88069c7
Mariam <mariam.antar@hotmail.com>
2018-11-24 18:43:11 +1100

    schema

--- server/index.js
+++ server/index.js
@@ -1,18 +1,44 @@
-const { GraphQLServer } = require('graphql-yoga')
+const { GraphQLServer } = require('graphql-yoga');
+const mongoose = require('mongoose');
 
 
-// this is the schema
+mongoose.connect("mongodb://localhost/test5");
+
+const Todo = mongoose.model('Todo', {
+  text: String,
+  complete: Boolean
+});
+
+// this is the schema, ! means it's required
 const typeDefs = `
   type Query {
     hello(name: String): String!
   }
-`
+  type Todo {
+    id: ID!
+    text: String!
+    complete: Boolean!
+  }
+type Mutation {
+  createTodo(text: String!): Todo
+}
+`;
 
 const resolvers = {
   Query: {
     hello: (_, { name }) => `Hello ${name || 'World'}`,
   },
-}
+  Mutation: {
+    createTodo: async (_, {text}) => {
+      const todo = new Todo({text, complete: false});
+      await todo.save();
+      return todo;
+    }
+  }
+};
 
 const server = new GraphQLServer({ typeDefs, resolvers })
-server.start(() => console.log('Server is running on localhost:4000'))
+mongoose.connection.once('open', function() {
+  server.start(() => console.log('Server is running on localhost:4000'));
+  // we're connected
+});
